914d099ecc691c97f0096e8d30372ee6c04cc476
bloodwithmilk25 <humapen@gmail.com>
2018-09-29 18:24:21 +0300

    First commit

---
+++ django-populator/django_populator.py
@@ -0,0 +1,30 @@
+import xlrd
+
+
+class Populator:
+    def __init__(self, path: str, model, sheet=0):
+        self.path = path
+        self.model = model
+        self.sheet = sheet
+
+    def __str__(self):
+        return str(self.model)
+
+    def populator(self):
+        work_book = xlrd.open_workbook(self.path)
+        sheet = work_book.sheet_by_index(self.sheet)
+
+        # extracting column names
+        col_names = []
+        for cell in sheet.row(0):
+            col_names.append(cell.value)
+
+        # going through all the rows with data except the first one
+        for rownumber in range(sheet.nrows)[1:]:
+            row = sheet.row_values(rownumber)
+            entry = dict()  # accumulating data for one entry in dictionary
+            # col iterator
+            # index corresponds with col_name of a column value
+            for index, col in enumerate(row):
+                entry[col_names[index]] = col
+            self.model.objects.create(**entry)
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-populator",
+    version="0.0.1",
+    author="Nikita Tonkoshkur",
+    author_email="humapen@gmail.com",
+    description="Populate your Django model from .xls file",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
